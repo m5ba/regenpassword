89e00484d4c05cbe28376b4c9cce43eee1dcddc9
Mark Balonkin <m5ba@outlook.com>
2024-09-26 11:00:26 -0500

    complete dotnet app

---
+++ dotnet/ReGenPass/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReGenPass;
+
+Console.WriteLine("Hello, World! ReGenPass is here.");
+if(args.Length != 4)
+{
+    Console.WriteLine("Usage: regenpass.exe <resource> <login> <iteration> <secret>");
+    return;
+}
+PasswordContext passwordContext = new()
+{
+    Resource = args[0],
+    Login = args[1],
+    Iteration = Int32.Parse(args[2]),
+    Secret = args[3]
+};
+string pwd = PassComputer.Compute(passwordContext);
+Console.WriteLine("Computed Password:");
+Console.WriteLine(pwd);
